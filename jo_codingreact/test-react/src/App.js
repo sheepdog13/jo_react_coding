bc5cafb75d78780f578bfe14eff6b9e0290c7046
sheepdog13 <mikecho1234@naver.com>
2022-10-28 17:44:06 +0900

    1028 생활코딩 props state this bind event

---
+++ jo_codingreact/test-react/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import { Component } from 'react';
+import TOC from './components/TOC'
+import Content from './components/Content';
+import Subject from './components/Subject';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mode:'read',
+      subject:{title:'WEB', sub:'World wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      contents: [
+        {id:1, title:'HTML', desc:'HTML is for imformation'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+  render() {
+    console.log('App render')
+    var _title, _desc =null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    console.log('render',this)
+    return (
+      <div className='App'>
+        {/*<Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>*/}
+        <header>
+              <h1><a href='/' onClick={function(e){
+                console.assert('event in',this)
+                e.preventDefault();
+                return;
+                e.preventDefault();
+                //this.state.mode = 'welcome';
+                this.setState({
+                  mode:'welcome'
+                })
+              }}>{this.state.subject.title}</a></h1>
+              {this.state.subject.sub}
+        </header>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
+      </div>
+    );
+  }
+}
+
+export default App;
